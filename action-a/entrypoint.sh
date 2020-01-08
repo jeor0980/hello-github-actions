e8b3572b5a2f4caf21cd27e1d7b36ab47ac83aea
jeor0980 <jesusotovar@hotmail.com>
2020-01-08 13:00:44 -0700

    cleanup

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,23 +1,22 @@
-#!/bin/sh -l
+#!/bin/bash
 
+git config --global user.email 'osr_web_deploy@colorado.edu'
+git config --global user.name 'osrwebdeploy'
 
-  sh -c "git config --global user.email 'osr_web_deploy@colorado.edu'"
-  sh -c "git config --global user.name 'osrwebdeploy'"
-
-sh -c "echo Printing global variables..."
-sh -c "echo $GITHUB_REF"
-sh -c "echo $GITHUB_SHA"
-sh -c "echo Start script..."
-sh -c "git clone https://github.com/CuBoulder/express-mono-upstream.git"
-sh -c "cd express-mono-upstream"
-sh -c "git checkout -b $EXPRESS_VERSION"
-sh -c "cd profiles"
-sh -c "rm -rf express"
-sh -c "git clone https://github.com/CuBoulder/express_mono.git express"
-sh -c "cd express"
-sh -c "git checkout tags/$EXPRESS_VERSION"
-sh -c "cd .."
-sh -c "git add express"
-sh -c "git commit -m "Updating express_mono to $EXPRESS_VERSION""
-sh -c "git push --set-upstream origin $EXPRESS_VERSION"
-sh -c "echo End Script..."
+echo Printing global variables...
+echo $GITHUB_REF
+echo $GITHUB_SHA
+echo Start script...
+git clone https://github.com/CuBoulder/express-mono-upstream.git
+cd express-mono-upstream
+git checkout -b $EXPRESS_VERSION
+cd profiles
+rm -rf express
+git clone https://github.com/CuBoulder/express_mono.git express
+cd express
+git checkout tags/$EXPRESS_VERSION
+cd .."
+git add express
+git commit -m "Updating express_mono to $EXPRESS_VERSION
+git push --set-upstream origin $EXPRESS_VERSION
+echo End Script...
