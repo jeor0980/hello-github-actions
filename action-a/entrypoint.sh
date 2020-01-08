e85b2dbf23246bf7bc32fcc46c846fc9e200cf13
jeor0980 <jesusotovar@hotmail.com>
2020-01-08 11:56:16 -0700

    Adding missing echo's

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-sh -c "Printing global variables..."
+sh -c "echo Printing global variables..."
 sh -c "echo $GITHUB_REF"
 sh -c "echo $GITHUB_SHA"
-sh -c "Start script..."
+sh -c "echo Start script..."
 sh -c "git clone git@github.com:CuBoulder/express-mono-upstream.git"
 sh -c "git checkout -b $EXPRESS_VERSION"
 sh -c "cd profiles"
@@ -15,4 +15,4 @@ sh -c "cd .."
 sh -c "git add express"
 sh -c "git commit -m "Updating express_mono to $EXPRESS_VERSION""
 sh -c "git push --set-upstream origin $EXPRESS_VERSION"
-sh -c "End Script..."
+sh -c "echo End Script..."
