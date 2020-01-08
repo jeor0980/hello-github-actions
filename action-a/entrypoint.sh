31933b5ab980fa4437f1d0e624da49fcb9e6880d
jeor0980 <jesusotovar@hotmail.com>
2020-01-08 11:40:32 -0700

    upstream changes

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $MY_NAME"
+sh -c "Printing global variables..."
+echo GITHUB_REF
+echo GITHUB_SHA
+
+sh -c "Start script..."
+
+git clone git@github.com:CuBoulder/express-mono-upstream.git
+git checkout -b EXPRESS_VERSION
+cd profiles
+rm -rf express
+git clone git@github.com:CuBoulder/express_mono.git express
+cd express
+git checkout tags/$EXPRESS_VERSION
+cd ..
+git add express
+git commit -m "Updating express_mono to $EXPRESS_VERSION"
+git push --set-upstream origin EXPRESS_VERSION
+
+sh -c "End Script..."
