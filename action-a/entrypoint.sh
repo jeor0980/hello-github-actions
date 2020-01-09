a626e0f3bf186221c9d7fbf6a5bc5868074fe13d
jeor0980 <jesusotovar@hotmail.com>
2020-01-09 09:21:58 -0700

    Updating cloning steps

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-git config --global user.email 'osr_web_deploy@colorado.edu'
-git config --global user.name 'osrwebdeploy'
-
 echo Printing global variables...
 echo $GITHUB_REF
 echo $GITHUB_SHA
 echo Start script...
-git clone https://github.com/CuBoulder/express-mono-upstream.git
+echo Cloning express-mono-upstream
+git clone https://osrwebdeploy:<MYTOKEN>git@github.com/CuBoulder/express-mono-upstream.git
 cd express-mono-upstream
 git checkout -b $EXPRESS_VERSION
 ls -al
 cd profiles
 rm -rf express
-git clone https://github.com/CuBoulder/express_mono.git express
+echo Cloning express_mono
+git clone https://osrwebdeploy:<MYTOKEN>git@github.com/CuBoulder/express_mono.git express
 cd express
 git checkout $EXPRESS_VERSION
 cd ..
 git add express
 git commit -m "Updating express_mono to $EXPRESS_VERSION"
+echo Pushing changes...
 git push --set-upstream origin $EXPRESS_VERSION
 echo End Script...
