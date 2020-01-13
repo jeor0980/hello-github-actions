6fbbb069550877ac6ca1cbafb6efae8294892050
jeor0980 <jesusotovar@hotmail.com>
2020-01-13 11:57:57 -0700

    test 12

--- action-update-express-upstream/entrypoint.sh
+++ action-update-express-upstream/entrypoint.sh
@@ -3,7 +3,7 @@
 git config --global user.email "osr_web_deploy@colorado.edu"
 git config --global user.name "osrwebdeploy"
 
-TESTING_PREFIX="11-actions-test-"
+TESTING_PREFIX="12-actions-test-"
 UPSTREAM_NAME=$TESTING_PREFIX$EXPRESS_VERSION
 
 
@@ -44,6 +44,6 @@ TITLE="Actions Update express_mono to ${EXPRESS_VERSION}";   # pull request titl
 DATA="{\"title\":\"${TITLE}\", \"body\":\"${BODY}\", "base":"master", \"head\":\"${UPSTREAM_NAME}\"}";
 
 echo Sending pull request
-curl -sSL -H "Authorization: token ${TOKEN}" -H "application/vnd.github.v3+json" -X POST --data ${DATA} https://api.github.com/repos/CuBoulder/express-mono-upstream/pulls
+curl -sSL -H "Authorization: token ${TOKEN}" -H "application/vnd.github.v3+json" -X POST --data "${DATA}" https://api.github.com/repos/CuBoulder/express-mono-upstream/pulls
 
 echo End Script...
