9f4506c26339872e531847504728b6ea4b971e93
jeor0980 <jesusotovar@hotmail.com>
2020-01-13 11:04:45 -0700

    test 8

--- action-update-express-upstream/entrypoint.sh
+++ action-update-express-upstream/entrypoint.sh
@@ -3,7 +3,7 @@
   git config --global user.email "osr_web_deploy@colorado.edu"
   git config --global user.name "osrwebdeploy"
 
-  TESTING_PREFIX = "5-actions-test-"
+  TESTING_PREFIX = "8-actions-test-"
   UPSTREAM_NAME = $TESTING_PREFIX += $EXPRESS_VERSION
 
 echo Printing global variables...
@@ -29,15 +29,15 @@ echo Pushing changes...
 git push --set-upstream origin $UPSTREAM_NAME
 
 # Workaround for storing JSON as bash variable without external library
-DATA=$(cat <<EOF
-'{"title": "Actions Update express_mono to",
-"body": "Please pull these awesome changes in",
-"head": "5-actions-test-v4.0.0",
-"base": "master"}'
-EOF
-)
+# DATA=$(cat <<EOF
+# '{"title": "Actions Update express_mono to",
+# "body": "Please pull these awesome changes in",
+# "head": "5-actions-test-v4.0.0",
+# "base": "master"}'
+# EOF
+# )
 
 echo Sending pull request
-curl -sSL -H "Authorization: token ${TOKEN}" -H "application/vnd.github.v3+json" -X POST --data ${DATA} https://api.github.com/repos/CuBoulder/express-mono-upstream/pulls
+curl -sSL -H "Authorization: token ${TOKEN}" -H "application/vnd.github.v3+json" -X POST --data '{"title": "Actions Update express_mono to", "body": "Please pull these awesome changes in", "head": "8-actions-test-v4.0.0", "base": "master"}' https://api.github.com/repos/CuBoulder/express-mono-upstream/pulls
 
 echo End Script...
